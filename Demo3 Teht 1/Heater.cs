da1b37151c14f43bbcc59c0a93f3bee852b6c513
Saamari <sampsa_kautto@hotmail.com>
2016-01-19 10:58:25 +0200

    Heater

---
+++ Demo3 Teht 1/Heater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Teht_1
+{
+    class Heater
+    {
+        // properties
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+
+        //method prints heater data
+
+        public void PrintData()
+        {
+            Console.WriteLine("Heater");
+            Console.WriteLine(" - Temperature : {0} Humidity {1} Is on {2}", Temperature, Humidity, IsOn);
+
+        }
+        //method returns heater data
+        public override string ToString()
+        {
+            // base.ToString();
+
+            return "Temperature" + Temperature + "Humidity" + Humidity;
+        }
+       
+
+        
+    }
+}
---
+++ Demo3 Teht 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Teht_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create new Heater object
+            Heater heater = new Heater();
+            heater.IsOn = true;
+            heater.Temperature = 100;
+            heater.Humidity = 95;
+            heater.PrintData();
+           
+
+            Console.ReadLine();
+        }
+    }
+}
